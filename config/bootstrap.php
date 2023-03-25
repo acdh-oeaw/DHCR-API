05b5bd43149b99e50796f90d48ff0ff4d93d5675
patrickakk <patrick@algority.eu>
2023-03-25 03:07:50 +0100

    disabled use of immutable

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -181,14 +181,19 @@ ServerRequest::addDetector('tablet', function ($request) {
  * locale specific date formats. For details see
  * @link https://book.cakephp.org/3.0/en/core-libraries/internationalization-and-localization.html#parsing-localized-datetime-data
  */
-Type::build('time')
-    ->useImmutable();
-Type::build('date')
-    ->useImmutable();
-Type::build('datetime')
-    ->useImmutable();
-Type::build('timestamp')
-    ->useImmutable();
+
+ /*  PA 25-03-2023: Disabled the use of immutable
+ *   Configuring immutable or mutable classes is deprecated and immutable classes will be the permanent configuration in 5.0. 
+ *   Calling `useImmutable()` is unnecessary.
+ */
+// Type::build('time')
+//     ->useImmutable();
+// Type::build('date')
+//     ->useImmutable();
+// Type::build('datetime')
+//     ->useImmutable();
+// Type::build('timestamp')
+//     ->useImmutable();
 
 /*
  * Custom Inflector rules, can be set to correctly pluralize or singularize
