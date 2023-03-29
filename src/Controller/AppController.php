63f75181db00970930d0eece5b440386ffe98047
patrickakk <patrick@algority.eu>
2023-03-29 11:12:54 +0200

    refactored initalize in AppController

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -41,7 +41,7 @@ class AppController extends Controller
      *
      * @return void
      */
-    public function initialize()
+    public function initialize(): void
     {
         parent::initialize();
 
