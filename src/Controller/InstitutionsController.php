68062859af36c41c2fd00f4872b00ab3f01de9d1
patrickakk <patrick@algority.eu>
2023-03-30 08:45:33 +0200

    reformat

--- src/Controller/InstitutionsController.php
+++ src/Controller/InstitutionsController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use App\Controller\AppController;
@@ -19,14 +20,15 @@ class InstitutionsController extends AppController
      *
      * @return \Cake\Http\Response|void
      */
-	public function index() {
-		$this->Institutions->evaluateQuery($this->request->getQuery());
+    public function index()
+    {
+        $this->Institutions->evaluateQuery($this->request->getQuery());
 
-		$institutions = $this->Institutions->getInstitutions();
+        $institutions = $this->Institutions->getInstitutions();
 
-		$this->set('institutions', $institutions);
-		$this->set('_serialize', 'institutions');
-	}
+        $this->set('institutions', $institutions);
+        $this->set('_serialize', 'institutions');
+    }
 
     /**
      * View method
@@ -35,12 +37,11 @@ class InstitutionsController extends AppController
      * @return \Cake\Http\Response|void
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
-	public function view($id = null) {
-		$institution = $this->Institutions->getInstitution($id);
+    public function view($id = null)
+    {
+        $institution = $this->Institutions->getInstitution($id);
 
-		$this->set('institution', $institution);
-		$this->set('_serialize', 'institution');
-	}
-
-
+        $this->set('institution', $institution);
+        $this->set('_serialize', 'institution');
+    }
 }
