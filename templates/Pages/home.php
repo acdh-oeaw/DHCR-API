24d064691d256b9ca1f431708890f255e7e5ec61
patrickakk <patrick@algority.eu>
2023-05-06 21:54:43 +0200

    api v1 - generic email notation

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -69,11 +69,7 @@ $this->layout = false;
                 <a href="https://dhcr.clarin-dariah.eu/info/#data" target="_blank">DHCR Info Page</a>
             </p>
             <h4>Support</h4>
-            <p>
-                <a href="mailto:patrick@clarin.eu?subject=[DHCR-API] Support" target="_blank">
-                    patrick@clarin.eu
-                </a>
-            </p>
+            <p>patrick /at/ clarin.eu</p>
         </div>
 
         <div class="columns large-6">
